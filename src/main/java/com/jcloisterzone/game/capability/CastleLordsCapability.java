d2ea17d6c6036ea97726e268224513151afe8544
stanolacko <stano.lacko@gmail.com>
2022-01-14 18:15:49 +0100

    Finished as FigureBonusPoints - need to append score expr.

--- src/main/java/com/jcloisterzone/game/capability/CastleLordsCapability.java
+++ src/main/java/com/jcloisterzone/game/capability/CastleLordsCapability.java
@@ -2,6 +2,7 @@ package com.jcloisterzone.game.capability;
 
 import com.jcloisterzone.Player;
 import com.jcloisterzone.XMLUtils;
+import com.jcloisterzone.board.Location;
 import com.jcloisterzone.board.Position;
 import com.jcloisterzone.board.Tile;
 import com.jcloisterzone.board.TileModifier;
@@ -12,6 +13,7 @@ import com.jcloisterzone.event.TokenReceivedEvent;
 import com.jcloisterzone.event.PlayEvent.PlayEventMeta;
 import com.jcloisterzone.event.ScoreEvent.ReceivedPoints;
 import com.jcloisterzone.feature.Circus;
+import com.jcloisterzone.feature.River;
 import com.jcloisterzone.feature.Road;
 import com.jcloisterzone.feature.Scoreable;
 import com.jcloisterzone.feature.modifier.BooleanAnyModifier;
@@ -22,7 +24,10 @@ import com.jcloisterzone.game.ScoreFeatureReducer;
 import com.jcloisterzone.game.capability.GoldminesCapability.GoldToken;
 import com.jcloisterzone.game.capability.LittleBuildingsCapability.LittleBuilding;
 import com.jcloisterzone.game.state.GameState;
+import com.jcloisterzone.game.state.PlacedTile;
 
+import io.vavr.Predicates;
+import io.vavr.Tuple;
 import io.vavr.Tuple2;
 import io.vavr.collection.LinkedHashMap;
 import io.vavr.collection.List;
@@ -47,76 +52,47 @@ public class CastleLordsCapability extends Capability<Void> {
             return bonusPoints;
         }
         Road road = (Road) feature;
-//System.out.println("Castle lords");
-//System.out.println(road.hasModifier(state, Road.CASTLE_LORDS));
+
         if (!road.hasModifier(state, Road.CASTLE_LORDS)) {
             return bonusPoints;
         }
+        
+        // Get All Castle Lords Tiles on Road
+        List<Road> castleLords = feature.getPlaces()
+                .map(fp -> {
+                     PlacedTile pt = state.getPlacedTile(fp.getPosition());
+                     return (Road) pt.getInitialFeaturePartOf(fp.getLocation())._2
+                         .placeOnBoard(fp.getPosition(), pt.getRotation());
+                }).filter(f -> ((Road) f).hasModifier(state, Road.CASTLE_LORDS));
 
-        // Find Castle Lords Tiles on Road 
-        var a = state.getPlacedTiles().filter((pos,pt) -> (road.getTilePositions().contains(pos)));// && pt.getTile().getInitialFeatures().filter((fp,f) -> fp.isPartOf(feature.getPlaces().get()) && ((Road) f).hasModifier(state, Road.CASTLE_LORDS)).size()>0));
-System.out.println(a);        
-		var b = feature.getPlaces()
-			.filter(
-				fp -> state.getPlacedTiles()
-//						.filter(
-//						(pos,pt) -> pt.getTile().getInitialFeatures().filter(
-//								(_fp,f) -> _fp.isPartOf(feature.getPlaces().get()/* && f instanceof Road && ((Road) f).hasModifier(state, Road.CASTLE_LORDS)).size()>0) */)
-//						).size()
-//						)
-				.size()>0
-			);
-
-        System.out.println(b);        
-		
-        return bonusPoints;
-        /*
-        Map<Position, LittleBuilding> buildings = state.getCapabilityModel(LittleBuildingsCapability.class);
-        if (buildings == null) {
-            return List.empty();
-        }
-        Set<Position> position = getTilePositions();
-        Seq<LittleBuilding> buldingsSeq = buildings.filterKeys(pos -> position.contains(pos)).values();
-
-        getTilePositions
-        // If more than one Castle Lord Rile, no bonus
-        if >1
+        // If more than one Castle Lord Tile, no bonus
+        if (castleLords.size()>1) {
         	return bonusPoints;
     	}
-    	
-    	// When there is at least one meeple not on CastleRoad, then no bonus
-    	Check meeples count and meeples on CastleLords tile
-    	if !=
-    		return bonusPoints;
-		}
-		
-		// All meeples are on CastleLords tile and get's bonus by majority
-		
-        Position cloisterPosition = monastery.getPlace().getPosition();
-        Set<Position> positions = Position.ADJACENT_AND_DIAGONAL.map(pt -> cloisterPosition.add(pt._2)).toSet().add(cloisterPosition);
-        Map<Player, LinkedHashMap<Meeple, FeaturePointer>> adjacentMeepleCount = state.getDeployedMeeples()
+
+        Position castleLordPosition = castleLords.get().getPlace().getPosition();
+        
+        LinkedHashMap<Meeple, FeaturePointer> followers = state.getDeployedMeeples()
                 .filter(mt -> mt._1 instanceof  Follower)
-                .filter(mt -> positions.contains(mt._2.getPosition()))
-                .groupBy(mt -> mt._1.getPlayer());
-                //.map((p, mts)-> new Tuple2<Player, Integer>(p, mts.size()));
-        int max = adjacentMeepleCount.values().map(map -> map.size()).max().getOrElse(-1);
-        Set<Player> players = adjacentMeepleCount.filter((p, map) -> map.size() == max).keySet();
+                .filter(mt -> mt._1.getPosition(state).equals(castleLordPosition));
 
-        for (Player player: players) {
-            LinkedHashMap<Meeple, FeaturePointer> followers = adjacentMeepleCount.get(player).get();
-            Tuple2<Meeple, FeaturePointer> onTile = followers.filter(t -> t._2.getPosition().equals(cloisterPosition)).getOrNull();
-            FeaturePointer fp;
-            if (onTile == null) {
-                // if user hasn't follower on monastery itself, use random follower from adjacent tile
-                fp = followers.get()._2;
-            } else {
-                fp = onTile._2;
-            }
+        // If meeples are not only on Castle Lord tile, no bonus
+        
+        if (!castleLords.get().getFollowers(state).equals(feature.getFollowers(state))) {
+        	return bonusPoints;
+        }
+        int size = road.getTilePositions().size();
+        
+        int castleLordsPoints = (size)*(size-1)/2;
 
-            PointsExpression expr = new PointsExpression("church", new ExprItem("church", CHURCH_TILES_BONUS));
+        // Bonus per player, not per meeple
+        for (Player player: feature.getOwners(state)) {
+        	FeaturePointer fp = followers.filter(t -> t._1.getPlayer().equals(player)).get()._2;
+            PointsExpression expr = new PointsExpression("castlelords", new ExprItem("castlelords", castleLordsPoints));
             bonusPoints = bonusPoints.append(new ReceivedPoints(expr, player, fp));
         }
 
-        return bonusPoints; */
+        return bonusPoints;
+
     }
 }
