0c9b27969ddd2934a2b8129a980aea2d5989cb8e
stanolacko <stano.lacko@gmail.com>
2025-04-16 20:05:33 +0200

    Checked setup, and simplified checking of possible tile placement

--- src/main/java/com/jcloisterzone/game/capability/FamiliesCapability.java
+++ src/main/java/com/jcloisterzone/game/capability/FamiliesCapability.java
@@ -4,13 +4,14 @@ import com.jcloisterzone.board.*;
 import com.jcloisterzone.feature.City;
 import com.jcloisterzone.feature.Feature;
 import com.jcloisterzone.feature.Structure;
-import com.jcloisterzone.feature.modifier.StringNonMergingNonEmptyModifier;
+import com.jcloisterzone.feature.modifier.StringStrictMergingModifier;
 import com.jcloisterzone.game.Capability;
 import com.jcloisterzone.game.setup.GameElementQuery;
 import com.jcloisterzone.game.state.GameState;
 import com.jcloisterzone.game.state.PlacedTile;
 import com.jcloisterzone.reducers.PlaceTile;
-import io.vavr.collection.List;
+
+import io.vavr.collection.Stream;
 
 import static com.jcloisterzone.XMLUtils.attributeIntValue;
 import static com.jcloisterzone.XMLUtils.attributeStringValue;
@@ -19,50 +20,45 @@ import org.w3c.dom.Element;
 
 public class FamiliesCapability extends Capability<PlacedTile> {
 
-	public static final StringNonMergingNonEmptyModifier FAMILY = new StringNonMergingNonEmptyModifier("family", null);
+	public static final StringStrictMergingModifier FAMILY = new StringStrictMergingModifier("family", new GameElementQuery("families"));
 
 	@Override
     public Feature initFeature(GameState state, String tileId, Feature feature, Element xml) {
-        if (new GameElementQuery("families").equals(true) != null) {
-            if (feature instanceof City) {
-        	    if (attributeIntValue(xml, "pennants", 0) > 0 ) {
-                    String family = "blue";
-                    if (xml.hasAttribute("family")) {
-                        family = xml.getAttribute("family");
-                    }
-                    feature = ((City) feature).putModifier(FAMILY, family);
-        	    }
-            }
-		}
+        if (feature instanceof City) {
+    	    if (attributeIntValue(xml, "pennants", 0) > 0 ) {
+                String family = "blue";
+                if (xml.hasAttribute("family")) {
+                    family = xml.getAttribute("family");
+                }
+                feature = ((City) feature).putModifier(FAMILY, family);
+    	    }
+        }
         return feature;
     }
 
 	@Override
     public boolean isTilePlacementAllowed(GameState state, Tile tile, PlacementOption placement) {
-        Position pos = placement.getPosition();
+		
+		if (!(FAMILY.getEnabledBy() != null && FAMILY.getEnabledBy().apply(state))) return true;
+		     // Skip testing if FAMILY is not initalized by setup
+
+		Position pos = placement.getPosition();
         Rotation rot = placement.getRotation();
 
-        state = (new PlaceTile(tile, pos, rot)).apply(state);
+        final GameState finalState = (new PlaceTile(tile, pos, rot)).apply(state);
 
-        List<City> cities = state.getTileFeatures2(pos, Structure.class)
-                .filter(fp -> City.class.isInstance(fp._2))
-        		.map(t -> (City) t._2)
-           		.toList();
-
-        if (cities.size()==0) {
-        	// No city
+        Stream<City> cities = finalState.getTileFeatures2(pos, Structure.class)
+        	    .filter(fp -> fp._2 instanceof City && ((City) fp._2).getModifier(finalState, City.PENNANTS, 0) > 0)
+        	    .map(fp -> (City) fp._2)
+        	    .toStream();
+        
+        if (cities.isEmpty()) {
+        	// No city with Pennant, placement allowed
         	return true;
         }
         
-        for(City city : cities) {
-        	if (city.getModifier(state, City.PENNANTS, 0) > 0) {
-        		if (city.getModifier(state, FAMILY, null).equals("")) {
-        			// Family is null, not allowed placement, because families joined due to StringNonMergingModifier = null
-        			return false;
-        		}
-        	}
-        }
-        
-        return true;
+        return !cities.exists(city ->
+            city.getModifier(finalState, FAMILY, null).equals(StringStrictMergingModifier.CONFLICT)
+        );
     }
 }
