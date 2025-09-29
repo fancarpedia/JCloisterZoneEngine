358b9528a59688231217113c96f567afd6385c56
stanolacko <stano.lacko@gmail.com>
2025-09-29 19:48:29 +0200

    MultisetAddMofigier for features to count different string modifiers for feature when merged

---
+++ src/main/java/com/jcloisterzone/feature/modifier/MultisetAddModifier.java
@@ -0,0 +1,31 @@
+package com.jcloisterzone.feature.modifier;
+
+import com.jcloisterzone.game.setup.SetupQuery;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Map;
+
+public class MultisetAddModifier extends FeatureModifier<Map<String, Integer>> {
+
+    public MultisetAddModifier(String selector, SetupQuery enabledBy) {
+        super(selector, enabledBy);
+    }
+
+    @Override
+    public Map<String, Integer> mergeValues(Map<String, Integer> a, Map<String, Integer> b) {
+        if (a == null) return b;
+        if (b == null) return a;
+        return a.merge(b, Integer::sum); // adds counts for common keys
+    }
+
+    @Override
+    public Map<String, Integer> valueOf(String attr) {
+        if (attr == null || attr.isBlank()) {
+            return HashMap.empty();
+        }
+        Map<String, Integer> result = HashMap.empty();
+        for (String token : attr.split("\\s+")) {
+            result = result.put(token, result.get(token).getOrElse(0) + 1);
+        }
+        return result;
+    }
+}
