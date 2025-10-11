8c00a8a1b791588d75a4bfca518f99f5fa551f75
stanolacko <stano.lacko@gmail.com>
2025-10-11 12:43:53 +0200

    Clean TileBuilder as changed Flowers on Road/City

--- src/main/java/com/jcloisterzone/board/TileBuilder.java
+++ src/main/java/com/jcloisterzone/board/TileBuilder.java
@@ -27,8 +27,8 @@ public class TileBuilder {
     protected final transient Logger logger = LoggerFactory.getLogger(getClass());
 
     private static final FeatureModifier[] MONASTERY_MODIFIERS = new FeatureModifier[] { Monastery.SPECIAL_MONASTERY, Monastery.SHRINE, Monastery.CHURCH };
-    private static final FeatureModifier[] CITY_MODIFIERS = new FeatureModifier[] { City.PENNANTS, City.CATHEDRAL, City.PRINCESS, City.BESIEGED, City.DARMSTADTIUM, City.POINTS_MODIFIER, City.FLOWERS };
-    private static final FeatureModifier[] ROAD_MODIFIERS = new FeatureModifier[] { Road.INN, Road.LABYRINTH, Road.ROBBERS_SON, Road.WELL, Road.FLOWERS };
+    private static final FeatureModifier[] CITY_MODIFIERS = new FeatureModifier[] { City.PENNANTS, City.CATHEDRAL, City.PRINCESS, City.BESIEGED, City.DARMSTADTIUM, City.POINTS_MODIFIER };
+    private static final FeatureModifier[] ROAD_MODIFIERS = new FeatureModifier[] { Road.INN, Road.LABYRINTH, Road.ROBBERS_SON, Road.WELL };
     private static final FeatureModifier[] FIELD_MODIFIERS = new FeatureModifier[] { Field.FLOWERS };
 
     private java.util.List<FeatureModifier> externalModifiers;
