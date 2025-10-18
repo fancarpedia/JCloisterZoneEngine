8b618de0c4d821a3f8ef3a79ac0c6b28ebd716ce
stanolacko <stano.lacko@gmail.com>
2025-10-18 13:39:11 +0200

    Marketplace scoring pointer set to scoring road meeple

--- src/main/java/com/jcloisterzone/game/capability/MarketplaceCapability.java
+++ src/main/java/com/jcloisterzone/game/capability/MarketplaceCapability.java
@@ -12,12 +12,14 @@ import com.jcloisterzone.feature.Completable;
 import com.jcloisterzone.feature.Marketplace;
 import com.jcloisterzone.feature.Scoreable;
 import com.jcloisterzone.feature.Road;
+import com.jcloisterzone.figure.Follower;
 import com.jcloisterzone.game.Capability;
 import com.jcloisterzone.game.state.GameState;
 
 import io.vavr.collection.HashSet;
 import io.vavr.collection.List;
 import io.vavr.collection.Set;
+import io.vavr.Tuple2;
 
 import java.util.ArrayList;
 
@@ -48,9 +50,10 @@ public class MarketplaceCapability extends Capability<Void> {
     				tiles-= road.getTilePositions().size(); // Exclude scored road tiles
     			}
                 ArrayList<ExprItem> exprItems = new ArrayList<ExprItem>();
-                ExprItem expr = new ExprItem(tiles, "marketplace", tiles);
+                ExprItem expr = new ExprItem(tiles, "tiles", tiles);
                 for (Player player: owners) {
-                    bonusPoints = bonusPoints.append(new ScoreEvent.ReceivedPoints(new PointsExpression("marketplace", expr), player, new ScorePositionsFeaturePointer(fps.get(), positions))); // Positions or Feature pointer
+                    List<Tuple2<Follower, FeaturePointer>> followers = feature.getFollowers2(state).filter(t -> t._1.getPlayer().equals(player)).toList();
+                    bonusPoints = bonusPoints.append(new ScoreEvent.ReceivedPoints(new PointsExpression("marketplace", expr), player, new ScorePositionsFeaturePointer(followers.get()._2, positions))); // Positions or Feature pointer
                 }
         	}
         }
