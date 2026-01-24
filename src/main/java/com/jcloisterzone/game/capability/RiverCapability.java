365a7d1e5947cffcd45a53b9910d7a99f5aac48b
stanolacko <stano.lacko@gmail.com>
2026-01-24 15:04:34 +0100

    River check if streight line is not in collision with placed tiles

--- src/main/java/com/jcloisterzone/game/capability/RiverCapability.java
+++ src/main/java/com/jcloisterzone/game/capability/RiverCapability.java
@@ -6,8 +6,10 @@ import com.jcloisterzone.game.Capability;
 import com.jcloisterzone.game.state.GameState;
 import com.jcloisterzone.game.state.PlacedTile;
 import com.jcloisterzone.random.RandomGenerator;
+import com.jcloisterzone.reducers.PlaceTile;
 import io.vavr.Predicates;
 import io.vavr.Tuple2;
+import io.vavr.collection.LinkedHashMap;
 import io.vavr.collection.List;
 
 public class RiverCapability extends Capability<Void> {
@@ -44,8 +46,18 @@ public class RiverCapability extends Capability<Void> {
         Position adjPos = pos.add(side);
         return state.getPlacedTiles().containsKey(adjPos);
     }
+    
+    private List<Location> getRiverLocations(Tile tile, Rotation rot) {
+    	List<Location> riverLocations = tile.getInitialFeatures()
+            .filterValues(Predicates.instanceOf(River.class))
+            .map(Tuple2::_1)
+            .map(fp -> fp.getLocation().rotateCW(rot))
+            .toList();
 
-    private boolean isContinuationFree(GameState state, Position pos, Location side) {
+	    return riverLocations;
+    }
+
+    private boolean isContinuationFree(GameState state, Position pos, Location side, Tile tile, Rotation rot) {
         Position adjPos = pos.add(side);
         Position adjPos2 = adjPos.add(side);
         List<Position> reservedTiles = List.of(
@@ -60,20 +72,64 @@ public class RiverCapability extends Capability<Void> {
         	return false;
         }
         
-        // Possible future testing like no river tile in streight line, also on adjacent tile left-right
-        // Check possible collisions with other open river branches
-        return true;
+        GameState _state = (new PlaceTile(tile, pos, rot)).apply(state);
+
+        LinkedHashMap<Position, PlacedTile> placedTiles = _state.getPlacedTiles();
+        
+        int minX = 0, maxX = 0, minY = 0, maxY = 0;
+
+        for (Position p : _state.getPlacedTiles().keySet()) {
+            int x = p.x;
+            int y = p.y;
+
+            if (x < minX) minX = x;
+            if (y < minY) minY = y;
+            if (x > maxX) maxX = x;
+            if (y > maxY) maxY = y;
+        }
+
+    	List<Position> check = List.empty();
+        
+    	if (side.equals(Location.N)) {
+			for(int y = minY; y<pos.y; y++) {
+	    		check = check.append(new Position(pos.x-1,y));
+	    		check = check.append(new Position(pos.x,y));
+	    		check = check.append(new Position(pos.x+1,y));
+			}
+    	} else if (side.equals(Location.E)) {
+			for(int x = pos.x+1; x<=maxX; x++) {
+				check = check.append(new Position(x,pos.y-1));
+				check = check.append(new Position(x,pos.y));
+				check = check.append(new Position(x,pos.y+1));
+			}
+    	} else if (side.equals(Location.S)) {
+			for(int y = pos.y+1; y<=maxY; y++) {
+	    		check = check.append(new Position(pos.x-1,y));
+	    		check = check.append(new Position(pos.x,y));
+	    		check = check.append(new Position(pos.x+1,y));
+			}
+    	} else if (side.equals(Location.W)) {
+			for(int x = minX; x<pos.x; x++) {
+				check = check.append(new Position(x,pos.y-1));
+				check = check.append(new Position(x,pos.y));
+				check = check.append(new Position(x,pos.y+1));
+			}
+		}
+		if (check.size()>0 && check.exists(p -> placedTiles.containsKey(p))) {
+		    return false;
+		}
+		
+        // Possible future testings
+        // TODO: River junction following river curve. Two open neighbouring river branches with same direction
+		
+		return true;
     }
 
     @Override
     public boolean isTilePlacementAllowed(GameState state, Tile tile, PlacementOption placement) {
         Position pos = placement.getPosition();
         Rotation rot = placement.getRotation();
-        List<Location> riverLocations = tile.getInitialFeatures()
-                .filterValues(Predicates.instanceOf(River.class))
-                .map(Tuple2::_1)
-                .map(fp -> fp.getLocation().rotateCW(rot))
-                .toList();
+        List<Location> riverLocations = getRiverLocations(tile, rot);
 
         if (riverLocations.size() == 0) {
             return true;
@@ -92,7 +148,7 @@ public class RiverCapability extends Capability<Void> {
             if (sides.size() != openSidesSize) {
             	foundValidRiverPlacement = true;
 
-            	if (openSides.filter(side -> isContinuationFree(state, pos, side)).size() != openSidesSize) {
+            	if (openSides.filter(side -> isContinuationFree(state, pos, side, tile, rot)).size() != openSidesSize) {
 	            	return false;
 	            }
             }
