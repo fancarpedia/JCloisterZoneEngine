78a0d219b03e5ded6a13ce20553b46a507bab272
stanolacko <stano.lacko@gmail.com>
2026-02-10 17:45:17 +0100

    Watchtower scoring bullet for specific  meeple and considered tiles

--- src/main/java/com/jcloisterzone/game/capability/WatchtowerCapability.java
+++ src/main/java/com/jcloisterzone/game/capability/WatchtowerCapability.java
@@ -3,9 +3,11 @@ package com.jcloisterzone.game.capability;
 import com.jcloisterzone.board.Position;
 import com.jcloisterzone.board.TileModifier;
 import com.jcloisterzone.board.pointer.FeaturePointer;
+import com.jcloisterzone.board.pointer.ScoreMeeplePositionsPointer;
 import com.jcloisterzone.event.ExprItem;
 import com.jcloisterzone.event.PointsExpression;
 import com.jcloisterzone.event.ScoreEvent;
+import com.jcloisterzone.event.ScoreEvent.ReceivedPoints;
 import com.jcloisterzone.feature.*;
 import com.jcloisterzone.figure.Follower;
 import com.jcloisterzone.figure.Meeple;
@@ -13,9 +15,11 @@ import com.jcloisterzone.game.Capability;
 import com.jcloisterzone.game.state.GameState;
 import com.jcloisterzone.game.state.PlacedTile;
 import com.jcloisterzone.reducers.AddPoints;
+
 import io.vavr.Predicates;
 import io.vavr.Tuple2;
 import io.vavr.collection.List;
+import io.vavr.collection.Set;
 import io.vavr.collection.Stream;
 
 import java.util.ArrayList;
@@ -49,14 +53,14 @@ public class WatchtowerCapability  extends Capability<Void> {
             return state;
         }
 
-        for (var t : state.getDeployedMeeples()) {
+        for (Tuple2<Meeple, FeaturePointer> t : state.getDeployedMeeples()) {
             FeaturePointer fp = t._2;
             if (!watchtowers.containsKey(fp.getPosition())) {
                 continue;
             }
 
             if (features.stream().filter(f -> f.getPlaces().contains(fp)).findAny().isPresent()) {
-                var meeples = watchtowerMeeples.get(fp.getPosition());
+            	java.util.List<Tuple2<Meeple, FeaturePointer>> meeples = watchtowerMeeples.get(fp.getPosition());
                 if (meeples == null) {
                     meeples = new ArrayList<>();
                     watchtowerMeeples.put(fp.getPosition(), meeples);
@@ -65,18 +69,21 @@ public class WatchtowerCapability  extends Capability<Void> {
             }
         }
 
-        for (var entry : watchtowerMeeples.entrySet()) {
+        for (java.util.Map.Entry<Position, java.util.List<Tuple2<Meeple, FeaturePointer>>> entry : watchtowerMeeples.entrySet()) {
             Position pos = entry.getKey();
             WatchtowerModifier watchtower = watchtowers.get(pos);
             int count = 0;
             String exprName = null;
 
             GameState _state = state;
-            switch (watchtower.subject) {
+        	Stream<PlacedTile> pts = _state
+                	.getAdjacentAndDiagonalTiles(pos)
+                	.append(_state.getPlacedTile(pos));
+        	
+        	Set<Position> positions = pts.map(t -> t.getPosition()).toSet();
+
+        	switch (watchtower.subject) {
                 case "coat-of-arms":
-                	Stream<PlacedTile> pts = _state
-                    	.getAdjacentAndDiagonalTiles(pos)
-                    	.append(_state.getPlacedTile(pos));
                 	for(PlacedTile pt: pts) {
                 	  for (Feature f: pt.getTile().getInitialFeatures().values()) {
                         if (f instanceof City) {
@@ -112,7 +119,7 @@ public class WatchtowerCapability  extends Capability<Void> {
 
             if (count > 0) {
                 PointsExpression expr = new PointsExpression("watchtower", new ExprItem(count, exprName, count * watchtower.points));
-                var receivedPoints= List.ofAll(entry.getValue()).map(t -> new ScoreEvent.ReceivedPoints(expr, t._1.getPlayer(), t._2));
+                List<ReceivedPoints> receivedPoints = List.ofAll(entry.getValue()).map(t -> new ScoreEvent.ReceivedPoints(expr, t._1.getPlayer(), new ScoreMeeplePositionsPointer(t._1.getDeployment(_state), t._1.getId(), positions)));
                 state = (new AddPoints(receivedPoints, true)).apply(state);
             }
         }
@@ -127,7 +134,6 @@ public class WatchtowerCapability  extends Capability<Void> {
     }
 
     private Stream<Feature> getNeigbouringFeatures(GameState state, Position pos) {
-        //return getNeigbouring(state, pos).flatMap(pt -> pt.getTile().getInitialFeatures())
         return getNeigbouring(state, pos).flatMap(pt -> pt.getTile().getInitialFeatures().values());
     }
 }
