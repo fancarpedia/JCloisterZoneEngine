8700ba1ac8bb42da96fdf90c2014c30e5ecd2561
stanolacko <stano.lacko@gmail.com>
2026-05-13 15:08:25 +0200

    Rewrited CourierPhase for current variant of rules

--- src/main/java/com/jcloisterzone/game/phase/CourierPhase.java
+++ src/main/java/com/jcloisterzone/game/phase/CourierPhase.java
@@ -8,6 +8,8 @@ import com.jcloisterzone.event.PointsExpression;
 import com.jcloisterzone.event.MeepleDeployed;
 import com.jcloisterzone.event.ScoreEvent;
 import com.jcloisterzone.feature.Completable;
+import com.jcloisterzone.feature.Feature;
+import com.jcloisterzone.feature.Monastery;
 import com.jcloisterzone.feature.Scoreable;
 import com.jcloisterzone.figure.Follower;
 import com.jcloisterzone.figure.Meeple;
@@ -33,22 +35,20 @@ public class CourierPhase extends Phase {
     public StepResult enter(GameState state) {
         FeaturePointer fpCourier = state.getNeutralFigures().getCourierDeployment();
         
-        if (fpCourier.getPosition() != null) {
+        if (fpCourier != null && fpCourier.getPosition() != null) {
         	GameState _state = state;
         	
         	List<Tuple2<Meeple, FeaturePointer>> currentDeployedMeeples = state.getDeployedMeeples()
 	    		.filter(m -> m._1 instanceof Follower)
 	    		.filter(m -> m._2.getPosition().x == fpCourier.getPosition().x || m._2.getPosition().y == fpCourier.getPosition().y)
-	    		.filter(m -> Completable.class.isAssignableFrom(m._2.getFeature()))
-	    		.filter(m -> ((Completable) _state.getFeature(m._2)).isOpen(_state))
 	    		.toList();
 
         	MeepleDeployed courierMessageMeeple = List.ofAll(state.getCurrentTurnPartEvents())
 	            .filter(Predicates.instanceOf(MeepleDeployed.class))
 	            .map(ev -> (MeepleDeployed) ev)
 	            .filter(ev -> currentDeployedMeeples
-	            		.filter(m -> m._1.equals(ev.getMeeple()) && m._2.equals(ev.getPointer()))
-	            		.nonEmpty())
+            		.filter(m -> m._1.equals(ev.getMeeple()) && m._2.equals(ev.getPointer()))
+            		.nonEmpty())
 	            .filter(ev -> ev.getPointer().getPosition().x == fpCourier.getPosition().x || ev.getPointer().getPosition().y == fpCourier.getPosition().y)
 	            .getOrNull();
 
@@ -59,16 +59,27 @@ public class CourierPhase extends Phase {
             	
             	Position pos = courierMessageMeeple.getPointer().getPosition();
             	
-//TODO
             	List<Tuple2<Meeple, FeaturePointer>> affectedMeeples = state.getDeployedMeeples().filter((m, fp) -> {
                     if (!(m instanceof Follower)) return false;
+                    Feature f = _state.getFeature(fp);
+                    if (!(f instanceof Completable)) return false;
+                    if ((f instanceof Monastery) && (((Monastery) f).isSpecialMonastery(_state))) return false;
                     Position mpos = fp.getPosition();
                     return Math.abs(pos.x - mpos.x) <= 1 && Math.abs(pos.y - mpos.y) <= 1;
-                }).toList().distinctBy(Tuple2::_2);
+                })
+    			.toList()
+    		    .foldLeft(
+    		        List.<Tuple2<Meeple, FeaturePointer>>empty(),
+    		        (acc, t) -> acc.exists(existing -> t._2().isPartOf(existing._2()) || existing._2().isPartOf(t._2()))
+    		            ? acc
+    		            : acc.append(t)
+    		    );
 
-                Completable feature = (Completable) state.getFeature((FeaturePointer) courierMessageMeeple.getPointer());
-                ScoreEvent.ReceivedPoints points = new ScoreEvent.ReceivedPoints(feature.getPoints(state), courierMessageMeeple.getMeeple().getPlayer(), courierMessageMeeple.getMeeple().getDeployment(state));
-                state = (new AddPoints(points, false)).apply(state);
+            	for (Tuple2<Meeple, FeaturePointer> affectedMeeple: affectedMeeples) {
+            		Completable feature = (Completable) state.getFeature(affectedMeeple._2);
+            		ScoreEvent.ReceivedPoints points = new ScoreEvent.ReceivedPoints(feature.getPoints(state), courierMessageMeeple.getMeeple().getPlayer(), affectedMeeple._1.getDeployment(state));
+            		state = (new AddPoints(points, false)).apply(state);
+            	}
         	}
         }
 		return next(state);
