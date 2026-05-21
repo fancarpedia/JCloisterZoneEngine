83f4b7268e0499656e1828719936ae7a8d7c14e8
stanolacko <stano.lacko@gmail.com>
2026-05-21 18:48:25 +0200

    CourierPhase before End of First part of double turn

--- src/main/java/com/jcloisterzone/game/GameStatePhaseReducer.java
+++ src/main/java/com/jcloisterzone/game/GameStatePhaseReducer.java
@@ -42,9 +42,9 @@ public class GameStatePhaseReducer implements Function2<GameState, Message, Game
 
         next = cleanUpTurnPhase = new CleanUpTurnPhase(randomGenerator, null);
         if (setup.contains(BazaarCapability.class)) next = new BazaarPhase(randomGenerator, next);
-        if (setup.contains(CourierCapability.class)) next = new CourierPhase(randomGenerator, next);
         if (setup.getBooleanRule(Rule.ESCAPE)) next = new EscapePhase(randomGenerator, next);
         next = cleanUpTurnPartPhase = new CleanUpTurnPartPhase(randomGenerator, next);
+        if (setup.contains(CourierCapability.class)) next = new CourierPhase(randomGenerator, next);
         if (setup.contains(CornCircleCapability.class)) next = new CornCirclePhase(randomGenerator, next);
 
         if (setup.contains(DragonCapability.class) && "after-scoring".equals(setup.getStringRule(Rule.DRAGON_MOVEMENT))) {
