60a65b76a480a40ba59ffc69f441bc7b4812e97b
stanolacko <stano.lacko@gmail.com>
2026-05-23 16:28:32 +0200

    Added getRangeTilesWithFeature to RangeFeature

--- src/main/java/com/jcloisterzone/feature/RangeFeature.java
+++ src/main/java/com/jcloisterzone/feature/RangeFeature.java
@@ -4,8 +4,19 @@ import com.jcloisterzone.game.state.GameState;
 import com.jcloisterzone.game.state.PlacedTile;
 import io.vavr.collection.Stream;
 
-public interface RangeFeature {
+public interface RangeFeature extends Feature {
 
     Stream<PlacedTile> getRangeTiles(GameState state);
+    
+    default Stream<PlacedTile> getRangeTilesWithFeature(GameState state) {
+        Stream<PlacedTile> featureTiles = getPlaces()
+            .toStream()
+            .map(fp -> fp.getPosition())
+            .distinct()
+            .map(pos -> state.getPlacedTiles().get(pos))
+            .flatMap(opt -> opt.toStream());
+
+        return getRangeTiles(state).appendAll(featureTiles);
+    }
 
 }
